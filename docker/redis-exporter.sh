144843c7a9bbd3eba6cf8f8d851b955fbb95da84
golovanovsv <golovanovsv@gmail.com>
2025-08-28 12:07:22 +0500

    add haproxy compose

--- docker/redis-exporter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d --name redis-exporter \
-  --restart unless-stopped \
-  -p 9121:9121 \
-oliver006/redis_exporter \
-  -redis-only-metrics \
-  -redis.addr redis://redis:6379
